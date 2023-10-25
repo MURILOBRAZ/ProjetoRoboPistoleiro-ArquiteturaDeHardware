f2a5384181de41a0a4f4112150d0ab1e23a1ad6e
Felip3ra <felipesantanasantos1812@gmail.com>
2023-10-25 17:04:02 -0300

    fix: juntada de arquivos em um

--- app/main.ino
+++ app/main.ino
@@ -1,5 +1,20 @@
 #include <avr/io.h>
-#include "adc.ino"
+
+
+void InitADC(){
+
+	ADMUX |= (1 << REFS0);
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);
+}
+
+uint16_t ReadADC(uint8_t channel){
+
+	ADMUX = (ADMUX & 0XF0) | (channel & 0x0F);
+	
+	ADCSRA |= (1 << ADSC);
+	while(ADCSRA & (1 << ADSC));
+	return ADC;
+}
 
 int valorADC0 = 0;
 int valorADC1 = 0;
