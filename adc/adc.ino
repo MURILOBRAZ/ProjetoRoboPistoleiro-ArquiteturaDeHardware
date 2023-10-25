9f3c8341efac21c1e9cedb876191df73473b0e8d
Felip3ra <felipesantanasantos1812@gmail.com>
2023-10-25 16:53:51 -0300

    feat:implementação do ADC

---
+++ adc/adc.ino
@@ -0,0 +1,16 @@
+
+
+void InitADC(){
+
+	ADMUX |= (1 << REFS0);
+	ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADEN);
+}
+
+uint16_t ReadADC(uint8_t channel){
+
+	ADMUX = (ADMUX & 0XF0) | (channel & 0x0F);
+	
+	ADCSRA |= (1 << ADSC);
+	while(ADCSRA & (1 << ADSC));
+	return ADC;
+}
--- app/main.ino
+++ app/main.ino
@@ -1,17 +1,19 @@
 #include <avr/io.h>
-
+#include "adc.ino"
 
 int valorADC0 = 0;
 int valorADC1 = 0;
+int valorADC2 = 0;
 int valorMapeado0 = 0;
 int valorMapeado1 = 0;
 int main(void){
 	DDRD |= (1 << 2) | (1 << 3) | (1 << 7);
 	DDRD |= (1 << 4) | (1 << 5) | (1 << 6);
 	DDRB |= (1 << 0);
+    InitADC();
 	while(1){
 		
-		
+		valorADC0 = ReadADC(0);
 	}
 
 }
