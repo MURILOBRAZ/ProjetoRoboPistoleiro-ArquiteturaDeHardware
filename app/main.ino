cc727cb5693276df4862cd5424fbc84fdf893b4e
Felip3ra <felipesantanasantos1812@gmail.com>
2023-10-26 22:21:52 -0300

    "feat:Implementação do PWM - parte 1"

--- app/main.ino
+++ app/main.ino
@@ -1,6 +1,31 @@
 #include <avr/io.h>
 
 
+int valorADC0 = 0;
+int valorADC1 = 0;
+int valorMapeado0 = 0;
+int valorMapeado1 = 0;
+
+
+ISR(TIMER0_OVF_vect){
+	valorADC0 = ReadADC(0);
+	valorMapeado0 = map(valorADC0,0,1023,0,255);
+	OCR0A = valorMapeado0;
+}
+
+long map(long x, long in_min, long in_max, long out_min, long out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void Init_timer0(){
+
+	TCCR0A |= (1 << COM0A1) | (1 << WGM01) | (1 << WGM00);
+	TCCR0B |= (1 << CS00);
+	OCR0A = valorMapeado0;
+	TIMSK0 = (1 << TOIE0);
+	sei();
+}
+
 void InitADC(){
 
 	ADMUX |= (1 << REFS0);
@@ -16,19 +41,20 @@ uint16_t ReadADC(uint8_t channel){
 	return ADC;
 }
 
-int valorADC0 = 0;
-int valorADC1 = 0;
-int valorADC2 = 0;
-int valorMapeado0 = 0;
-int valorMapeado1 = 0;
+
+
 int main(void){
 	DDRD |= (1 << 2) | (1 << 3) | (1 << 7);
 	DDRD |= (1 << 4) | (1 << 5) | (1 << 6);
-	DDRB |= (1 << 0);
-    InitADC();
+	DDRB |= (1 << 0) | (1 << 3) | (1 << 4);
+	PORTD |= (1 << 6) | (1 << 7);
+	InitADC();
+	Init_timer0();
+	Serial.begin(9600);
 	while(1){
 		
-		valorADC0 = ReadADC(0);
+		Serial.println(valorADC0);
+		
 	}
 
 }
